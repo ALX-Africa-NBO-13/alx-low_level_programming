54efe86cc02760d1538d7812e52d8f0cc681d3bd
Miriam Naomi Anyango <minaoanya@gmail.com>
2023-03-01 02:39:23 -0800

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry poin
+ *
+ * Description: Print alphabets in small letters
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch == a; ch <= z; ch++)
+	{
+		putchar(ch);
+		putchar("\n");
+		getch();
+	}
+
+	return (0);
+}
