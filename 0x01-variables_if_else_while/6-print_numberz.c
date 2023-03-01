f47187018cac1da8a3bc01c52e54a21ee9664ac2
Miriam Naomi Anyango <minaoanya@gmail.com>
2023-03-01 14:17:49 -0800

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: All single numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
