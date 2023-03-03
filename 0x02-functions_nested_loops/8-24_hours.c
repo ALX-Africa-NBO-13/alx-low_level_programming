b49025d3796009ea380d37fb7990447d826b0703
Miriam Naomi Anyango <minaoanya@gmail.com>
2023-03-03 03:26:55 -0800

    done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * jack_bauer -  prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ * Description: well this is on another notch now, wow!
+ *
+ * Return: every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	
+}
