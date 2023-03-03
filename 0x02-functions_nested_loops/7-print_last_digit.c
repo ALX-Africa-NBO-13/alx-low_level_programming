ba7b06b629004552a1acb9ee4f0099806ee00a81
Miriam Naomi Anyango <minaoanya@gmail.com>
2023-03-03 02:38:11 -0800

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,14 +9,12 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
-
 	if (n < 0)
 	{
-		n = ((last_digit * -1));
+		n = n * -1;
 	}
 
-	_putchar(last_digit + '0');
+	_putchar((n % 10) + '0');
 
-	return (last_digit);
+	return (n % 10);
 }
