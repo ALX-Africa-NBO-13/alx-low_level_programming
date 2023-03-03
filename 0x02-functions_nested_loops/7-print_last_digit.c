3ac6695f484feb500737df164526bac33fda6fef
Miriam Naomi Anyango <minaoanya@gmail.com>
2023-03-03 02:30:53 -0800

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,12 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	_putchar(last_digit);
+	if (n < 0)
+	{
+		n = last_digit * -1;
+	}
+
+	_putchar(last_digit + '0');
 
 	return (last_digit);
 }
